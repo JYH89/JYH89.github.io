605ccdedc49282833acdfecabbff673f2b331348
铉嘉伟 <mifind@sina.com>
2018-07-16 15:10:28 +0800

    console

--- js/console.js
+++ js/console.js
@@ -1,3 +1,3 @@
-var str = "你好，这里是MIFind的console。能来到这里你就已经比全世界百分之九十五的人出色了，并且你用实际行动向我证明你真的对我很有意思，哈哈哈哈。这里是我的一些技术相关整理！\n我的微信：%cmystylexuan%c！\n你懂得😉~";
+var str = "你好，这里是MIFind的console。能来到这里你就已经比全世界百分之九十五的人出色了。这里是我的一些技术相关整理！\n我的微信：%cmystylexuan%c \n 请加我微信告诉我你为什么会找到这里，网络情缘一线牵，相信这份缘。😉";
 console.log("%c"+str+"\n%c    ","color: #6190e8;","padding: 0 5px;color: #fff;background: #6190e8;font-size:20px;",
     "color: #6190e8;","background: url('http://mifind.online/img/mifind.jpeg') no-repeat left center;background-size:100%;font-size: 100px;","\n\n");
